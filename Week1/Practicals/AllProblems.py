ea17e2eed19ddb689358cc2e7a1e9417613e1497
rafgrigag <grigoryanrafo@gmail.com>
2020-03-07 12:51:01 +0400

    practical1week1

---
+++ Week1/Practicals/AllProblems.py
@@ -0,0 +1,18 @@
+course = "Intro to Python"
+student = "Rafayel Grigoryan"
+nickname = "Superman"
+grade = 100
+PythonExpert=True
+print("course: "+ course)
+print("student: "+ student)
+print("nickname: "+ nickname)
+print("grade:",grade)
+print("PythonExpert:",PythonExpert)
+
+
+
+
+AB=3
+AC=4
+BC=(AB**2 + AC**2)**(1/2)
+print("The hypotenuse of the triangle ABC=",BC)
